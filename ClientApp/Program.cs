5337494d49cacd10a51c53ee7f3622d195e6843a
Ryan <ryanaringer1@gmail.com>
2021-11-17 23:45:59 -0800

    initial

---
+++ ClientApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            Console.Write("Enter your name: ");
+            person.Name = Console.ReadLine();
+
+            // Send the message  
+            string jsonPerson = System.Text.Json.JsonSerializer.Serialize(person);
+            byte[] bytes = sendMessage(System.Text.Encoding.Unicode.GetBytes(jsonPerson));
+        }
+        private static byte[] sendMessage(byte[] messageBytes)
+        {
+            byte[] responseBytes;
+            const int bytesize = 1024 * 1024;
+            try // Try connecting and send the message bytes  
+            {
+                System.Net.Sockets.TcpClient client = new System.Net.Sockets.TcpClient("192.168.1.220", 1234); // Create a new connection  
+                NetworkStream stream = client.GetStream();
+
+                stream.Write(messageBytes, 0, messageBytes.Length); // Write the bytes  
+                Console.WriteLine("================================");
+                Console.WriteLine("=   Connected to the server    =");
+                Console.WriteLine("================================");
+                Console.WriteLine("Waiting for response...");
+
+                responseBytes = new byte[bytesize]; // Clear the message   
+
+                // Receive the stream of bytes  
+                stream.Read(responseBytes, 0, responseBytes.Length);
+                Console.WriteLine(Encoding.Default.GetString(messageBytes));
+
+                // Clean up  
+                stream.Dispose();
+                client.Close();
+            }
+            catch (Exception e) // Catch exceptions  
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return messageBytes; // Return response  
+        }
+    }
+}
---
+++ ServerAppTest/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Net.Mail;
+using Newtonsoft.Json;
+
+namespace ServerAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ep = new IPEndPoint(GetLocalIPAddress(), 1234);
+            TcpListener listener = new TcpListener(ep);
+            listener.Start();
+
+            Console.WriteLine(@"  
+            ===================================================  
+                   Started listening requests at: {0}:{1}  
+            ===================================================",
+            ep.Address, ep.Port);
+
+            // Run the loop continuously; this is the server.  
+            while (true)
+            {
+                const int bytesize = 1024 * 1024;
+
+                string message = null;
+                byte[] buffer = new byte[bytesize];
+
+                var sender = listener.AcceptTcpClient();
+                sender.GetStream().Read(buffer, 0, bytesize);
+
+                // Read the message and perform different actions  
+                message = cleanMessage(buffer);
+
+                // Save the data sent by the client;  
+                Person person = JsonConvert.DeserializeObject<Person>(message); // Deserialize  
+                Console.WriteLine(person.Name);
+                byte[] bytes = System.Text.Encoding.Unicode.GetBytes("Thank you for your message, " + person.Name);
+                sender.GetStream().Write(bytes, 0, bytes.Length); // Send the response  
+                sendMessage(bytes, sender);
+            }
+        }
+        private static string cleanMessage(byte[] bytes)
+        {
+            string message = System.Text.Encoding.Unicode.GetString(bytes);
+
+            string messageToPrint = null;
+            foreach (var nullChar in message)
+            {
+                if (nullChar != '\0')
+                {
+                    messageToPrint += nullChar;
+                }
+            }
+            return messageToPrint;
+        }
+        public static IPAddress GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip;
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+        private static void sendMessage(byte[] bytes, TcpClient client)
+        {
+            client.GetStream()
+                .Write(bytes, 0,
+                bytes.Length); // Send the stream  
+        }
+    }
+}
